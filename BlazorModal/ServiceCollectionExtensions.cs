7f523d85ec22ad58d7f3bbd7022c0579d2f12b04
Keith <keithtmccartney@hotmail.com>
2020-10-21 21:26:13 +0100

    Applying changes to the Service Collection extension for adding referenced scope to the Modal Service.

--- BlazorModal/ServiceCollectionExtensions.cs
+++ BlazorModal/ServiceCollectionExtensions.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using BlazorModal.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorModal
 {
-	public class ServiceCollectionExtensions
+	public static class ServiceCollectionExtensions
 	{
+		public static IServiceCollection AddBlazorModal(this IServiceCollection services)
+		{
+			return services.AddScoped<ModalService>();
+		}
 	}
 }
